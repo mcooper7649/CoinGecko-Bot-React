6cc57effc90f23d8bc3852518073764065bb381e
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-11-18 16:03:22 -0500

    update footer with company branding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,12 @@
 const Footer = () => {
   return (
     <div className="footer container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-4 my-4 border-top">
-        <div className="d-flex align-items-center">
-          <a
-            href="/"
-            className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
-          >
-            <svg className="bi" width="30" height="24">
-              <use xlinkHref="#bootstrap"></use>
-            </svg>
-          </a>
-          <span className=" mb-3 mb-md-0">
-            © 2022 Crypto Price Bot | By MyCodeDojo
+      <footer className="justify-content-between align-items-center py-4 my-4 border-top">
+        <div className="d-flex align-items-center footer-items">
+          <span className="row mb-3 mb-md-0 copyright">
+            © 2022 Crypto Price Bot
           </span>
+          <img className="brand" src="bw-brand.webp" alt="developer" />
         </div>
       </footer>
     </div>
